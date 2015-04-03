b8f7d2dd0ea6db0ce5a154ce6dd78c1066f94782
IMOKURI <nenegi.01mo@gmail.com>
2015-04-03 23:24:10 +0900

    Use uptime function of sysinfo.h

--- src/System/Uptime.hs
+++ src/System/Uptime.hs
@@ -2,10 +2,19 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 
 module System.Uptime
-( c_uptime
+( upTime
 ) where
 
+import Foreign.C.Types
+import Foreign.Marshal.Alloc
+import Foreign.Ptr
 
-foreign import ccall "procps/proc/whattime.h print_uptime" c_uptime :: IO ()
+foreign import ccall "procps/proc/sysinfo.h uptime" c_upTime :: Ptr CDouble -> Ptr CDouble -> CInt
 
+upTime :: IO Int
+upTime = do
+  uptime_secs <- malloc :: IO (Ptr CDouble)
+  idle_secs   <- malloc :: IO (Ptr CDouble)
+
+  return $ fromIntegral $ c_upTime uptime_secs idle_secs
 
