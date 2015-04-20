7cfaad4612ba22e7a37348587158fefc89821ec3
IMOKURI <nenegi.01mo@gmail.com>
2015-04-20 13:53:42 +0900

    Add comment for haddock

--- src/System/Uptime.hs
+++ src/System/Uptime.hs
@@ -1,14 +1,28 @@
 
 {-# LANGUAGE ForeignFunctionInterface #-}
 
-module System.Uptime
-( sysUpTime
+{-|
+Module      : System.Uptime
+Description : System uptime library in Haskell
+Copyright   : 2015, IMOKURI
+License     : MIT
+Maintainer  : nenegi.01mo@gmail.com
+Stability   : experimental
+Portability : POSIX
+
+Library that returns system uptime in 1/100 seconds
+-}
+
+module System.Uptime (
+-- * Functions
+sysUpTime
 ) where
 
 import Foreign.C.Types
 
 foreign import ccall "get_uptime" c_getUpTime :: CLong
 
+-- | sysUpTime returns system uptime in 1/100 seconds
 sysUpTime :: IO Integer
 sysUpTime = return $ fromIntegral c_getUpTime
 
